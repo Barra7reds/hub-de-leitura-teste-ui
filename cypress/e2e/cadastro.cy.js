5c5c81c156d9f2a33963d216886a49ddde72dbb8
Gabriel Melo <eldergabriel603@gmail.com>
2026-02-24 16:23:17 -0300

    Page objects

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,10 +1,11 @@
 /// <reference types="cypress"/>
 import { faker } from '@faker-js/faker';
+import cadastroPage from '../support/pages/cadastro-page';
 
 describe('Funcionalidade: Cadastro de leitura', () => {
 
     beforeEach(() => {
-        cy.visit('register.html')
+        cadastroPage.visitarPaginaCadastro()
     });
 
     it('Deve fazer cadastro com sucesso, usando a função JS', () => {
@@ -32,6 +33,17 @@ describe('Funcionalidade: Cadastro de leitura', () => {
         cy.get('#register-btn').click()
         cy.url().should('include', 'dashboard')
         cy.get('#user-name').should('contain', nome)
+    });
+
+    it('Deve fazer cadastro com sucesso - Usando Page Objects', () => {
+        cadastroPage.preencherCadastro('Gabriel Melo', `teste${Date.now()}@teste.com`, '11999999999', 'teste123', 'teste123')
+        cy.url().should('include', 'dashboard')
+    });
+
+    it.only('Deve validar mensagens ao tentar cadastrar sem preencher o nome', () => {
+        cadastroPage.preencherCadastro ('', ``, '11992299999', 'teste123', 'teste123')
+        cy.get(':nth-child(1) > .invalid-feedback').should('contain', 'Nome deve ter pelo menos 2 caracteres')
+        cy.get('#register-form > :nth-child(2) > .invalid-feedback').should('contain', 'Email válido é obrigatório')
     });
 
     
--- cypress/e2e/catalogo-busca.cy.js
+++ cypress/e2e/catalogo-busca.cy.js
@@ -18,8 +18,21 @@ describe('Funcionalidade: Catálogo e Busca', () => {
         cy.get('.card-title').should('contain', catalogo[1].Livro)
     });
 
+    it('Deve fazer a busca de um livro usando Fixture', () => {
+        cy.fixture('livros').then((cat) => {
+            cy.get('#search-input').type(cat[0].Livro)
+            cy.get('.card-title').should('contain', cat[0].Livro)
+        })
+    });
 
-
+    it.only('Deve validar todos os livros da lista', () => {
+        cy.fixture('livros').then((cat) => {
+            cat.forEach(item => {
+                cy.get('#search-input').clear().type(item.Livro)
+                cy.get('.card-title').should('contain', item.Livro)
+            })
+        });
+    })
 
 
 });
